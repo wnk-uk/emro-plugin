edb7154330c8694a9aecddb421bc6ffa87f4d608
남궁욱 <dailycoding0211@gmail.com>
2025-04-16 19:34:47 +0900

    Completions fixing

--- src/main/java/com/emro/contributor/AutoCompletionContributor.java
+++ src/main/java/com/emro/contributor/AutoCompletionContributor.java
@@ -3,7 +3,9 @@ package com.emro.contributor;
 import com.emro.dictionary.LuceneManager;
 import com.emro.dictionary.LuceneManagerGlo;
 import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.codeInsight.lookup.LookupElementWeigher;
 import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiElement;
@@ -31,9 +33,29 @@ public class AutoCompletionContributor extends CompletionContributor {
 	                    SelectionModel selectionModel = parameters.getEditor().getSelectionModel();
 	                    String inputText = selectionModel.getSelectedText() != null ? selectionModel.getSelectedText() : "";
 
+
 		                if (inputText.isEmpty()) {
 			                inputText = getInputText(parameters);
 		                }
+
+	                    if (parameters.getInvocationCount() < 2 && !isKorean(inputText)) return;
+						if (inputText.indexOf(".") > -1 && !"STD.".equalsIgnoreCase(inputText)) return;
+
+
+	                    // 기본 sorter에 기반한 커스텀 sorter 생성
+	                    CompletionSorter sorter = CompletionSorter.defaultSorter(parameters, result.getPrefixMatcher())
+			                    .weighAfter("priority", new LookupElementWeigher("lucene-priority") {
+				                    @Override
+				                    public Comparable weigh(@NotNull LookupElement element) {
+					                    // PrioritizedLookupElement라면 priority 값을 사용
+					                    if (element instanceof PrioritizedLookupElement<?> ple) {
+						                    return -ple.getPriority();
+					                    }
+					                    return 0;
+				                    }
+			                    });
+
+	                    CompletionResultSet prioritizedResult = result.withRelevanceSorter(sorter);
 
                         if (isKorean(inputText)) {
 	                        List<Map<String, Object>> completions = null;
@@ -43,7 +65,7 @@ public class AutoCompletionContributor extends CompletionContributor {
                                 return;
 	                        }
 	                        for (Map<String, Object> completion : completions) {
-                                result.addElement(
+		                        prioritizedResult.addElement(
                                         PrioritizedLookupElement.withPriority(
                                                 LookupElementBuilder.create((String) completion.get("ko_KR"))
                                                         .withTailText((String) "-" + completion.get("en_US") + "/" +  completion.get("ko_KR"), true)
@@ -55,32 +77,28 @@ public class AutoCompletionContributor extends CompletionContributor {
                                                             String hitsText = (String) completion.get("key");
 //                                                            if ("용어집".equals(completion.get("source"))) hitsText = (String) completion.get("en_US");
                                                             con.getDocument().replaceString(startOffset, tailOffset, hitsText);
-                                                        }),-99999)
+                                                        }),-9999)
                                 );
                             }
-                            // 강제로 자동 트리거
-//                            result.restartCompletionOnAnyPrefixChange();
-//                            result.restartCompletionWhenNothingMatches();
-                        } else {
+
+                        } else if (parameters.getInvocationCount() >= 2) {
 							if (inputText.length() <= 1) return;
 
                             List<Map<String, Object>> completions = null;
+	                        List<Map<String, Object>> completionsKeys = null;
                             try {
                                 completions = LuceneManager.getInstance().search("en_US", inputText, "ngram2");
-	                            completions.addAll(LuceneManager.getInstance().search("key", inputText, "ngram2"));
+	                            completionsKeys =LuceneManager.getInstance().search("key", inputText, "ngram2");
                             } catch (Exception e) {
                                 return;
                             }
-	                        int i = -1000;
                             for (Map<String, Object> completion : completions) {
-								i--;
-                                result.addElement(
+	                            prioritizedResult.addElement(
                                         PrioritizedLookupElement.withPriority(
                                                 LookupElementBuilder.create((String) completion.get("en_US"))
-                                                        .withLookupString(((String) completion.get("key")).toLowerCase())
-                                                        .withLookupString(((String) completion.get("key")).toUpperCase())
+		                                                .withLookupString(((String) completion.get("en_US")).toUpperCase())
 		                                                .withLookupString(((String) completion.get("en_US")).toLowerCase())
-		                                                .withTailText((String) "-" + completion.get("en_US") + "/" +  completion.get("ko_KR"), true)
+		                                                .withTailText((String)/* "-" + completion.get("en_US") + "/" +  */completion.get("ko_KR"), true)
                                                         .withTypeText((String) completion.get("source"), true)
                                                         .withInsertHandler((con, item) -> {
                                                             // 사용자가 선택했을 때 삽입될 텍스트
@@ -89,9 +107,28 @@ public class AutoCompletionContributor extends CompletionContributor {
                                                             String hitsText = (String) completion.get("key");
 //                                                            if ("용어집".equals(completion.get("source"))) hitsText = (String) completion.get("en_US");
                                                             con.getDocument().replaceString(startOffset, tailOffset, hitsText);
-                                                        }),-99999)
+                                                        }),-9999)
                                 );
                             }
+
+	                        for (Map<String, Object> completion : completionsKeys) {
+		                        prioritizedResult.addElement(
+				                        PrioritizedLookupElement.withPriority(
+						                        LookupElementBuilder.create((String) completion.get("key"))
+								                        .withLookupString(((String) completion.get("key")).toLowerCase())
+								                        .withLookupString(((String) completion.get("key")).toUpperCase())
+								                        .withTailText((String) "-" + completion.get("en_US") + "/" +  completion.get("ko_KR"), true)
+								                        .withTypeText((String) completion.get("source"), true)
+								                        .withInsertHandler((con, item) -> {
+									                        // 사용자가 선택했을 때 삽입될 텍스트
+									                        int startOffset = con.getStartOffset();
+									                        int tailOffset = con.getTailOffset();
+									                        String hitsText = (String) completion.get("key");
+//                                                            if ("용어집".equals(completion.get("source"))) hitsText = (String) completion.get("en_US");
+									                        con.getDocument().replaceString(startOffset, tailOffset, hitsText);
+								                        }),-9999)
+		                        );
+	                        }
 
                             // 부모 요소가 특정 태그인지 검사
                             if (file.getName().endsWith(".java")) {
@@ -133,10 +170,12 @@ public class AutoCompletionContributor extends CompletionContributor {
         // 커서 이전 텍스트 추출 (최대 20자)
         int startOffset = Math.max(0, offset - 20);
         String textBeforeCursor = text.subSequence(startOffset, offset).toString();
+	    textBeforeCursor = textBeforeCursor.trim();
 
         // 공백이나 특수 문자 기준으로 가장 마지막 단어만 추출
         String[] tokens = textBeforeCursor.split("\\s+|<|>|\\(|\\)|\\{|\\}|\"|'");
-        return tokens.length > 0 ? tokens[tokens.length - 1] : "";
+        //return tokens.length > 0 ? tokens[tokens.length - 1] : "";
+	    return tokens.length > 0 ? String.join("", tokens) : "";
     }
 
     /**
